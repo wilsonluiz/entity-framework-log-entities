c88e689e0f49a7852a948317cfd8fba66623ec45
Wilson I Luiz <wilson@t2s.com.br>
2019-03-04 14:16:34 -0300

    Implementada unidade de trabalho

---
+++ src/Application/Dados/UnidadeDeTrabalho.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Application.Dados
+{
+    public interface IUnidadeDeTrabalho : IDisposable
+    {
+        void Finalizar();
+    }
+
+    public class UnidadeDeTrabalho<TContexto> : IUnidadeDeTrabalho where TContexto : DbContext
+    {
+        private TContexto _contexto;
+        private DbContextTransaction _transacao;
+
+        public UnidadeDeTrabalho(TContexto contexto)
+        {
+            _contexto = contexto;
+            _transacao = _contexto.Database.BeginTransaction();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Finalizar()
+        {
+            if (_transacao.UnderlyingTransaction.Connection == null) return;
+
+            _transacao.Commit();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+
+            if (_transacao != null)
+            {
+                _transacao.Dispose();
+                _transacao = null;
+            }
+
+            if (_contexto == null) return;
+
+            _contexto.Dispose();
+            _contexto = null;
+        }
+    }
+}
