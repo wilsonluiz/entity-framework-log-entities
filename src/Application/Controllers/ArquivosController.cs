6f579d501454ed9a432be7f1e5ce135cb77f7968
Wilson I Luiz <wilson@t2s.com.br>
2019-03-06 22:45:08 -0300

    Adicionado controller de arquivos responsável por gerenciar a leitura de arquivos e o envio das informações para a base de dado

---
+++ src/Application/Controllers/ArquivosController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Application.Dados;
+using Application.Dados.Entidades;
+using Application.Dados.Repositorios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [Route("api/arquivos")]
+    public class ArquivosController : ControllerBase
+    {
+        private const string NomeArquivo = "employees.json";
+        private readonly IRepositorioBase<HrContext, Arquivo> _repo;
+
+        public ArquivosController(IRepositorioBase<HrContext, Arquivo> repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListarArquivos()
+        {
+            var arquivos = await _repo.ListarTodosAssincrono();
+
+            return Ok(arquivos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> ObterArquivorId(string id)
+        {
+            var arquivo = await _repo.ObterPorIdAssincrono(id);
+
+            return Ok(arquivo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PesistirArquivos()
+        {
+            var diretorioBase = AppDomain.CurrentDomain.BaseDirectory;
+            var caminho = Path.Combine(diretorioBase, NomeArquivo);
+
+            using (var reader = new StreamReader(caminho))
+            {
+                var conteudo = reader.ReadToEnd();
+                var arquivo = new Arquivo
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    DataCriacao = DateTime.Now,
+                    Dados = conteudo
+                };
+
+                await _repo.AdicionarAssincrono(arquivo);
+
+                return Created("", arquivo);
+            }
+        }
+    }
+}
